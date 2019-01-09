6a4605b3ddf7497b39ede327c63816b6da75810b
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-09 21:26:05 +0000

    REFACTORED: div by 15 into private helper method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,7 @@
 class FizzBuzz
 
   def play(number)
-    if number % 15 == 0
+    if is_divisible_by_15(number)
       'FizzBuzz'
     elsif is_divisible_by_5(number)
       'Buzz'
@@ -14,6 +14,10 @@ class FizzBuzz
 
     def is_divisible_by_5(number)
       number % 5 == 0
+    end
+
+    def is_divisible_by_15(number)
+      number % 15 == 0
     end
 
 end
