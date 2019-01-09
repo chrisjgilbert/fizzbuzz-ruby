f02bdca0ea6c22f48ffbb8bc7c1c4271e0b970ac
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-09 21:19:08 +0000

    RED: play undefined

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,4 +2,10 @@ require 'fizzbuzz'
 
 describe FizzBuzz do
 
+  describe '#play' do
+    it 'returns Fizz if number is divisible by 3' do
+      expect(subject.play(3)).to eq 'Fizz'
+    end
+  end
+
 end
