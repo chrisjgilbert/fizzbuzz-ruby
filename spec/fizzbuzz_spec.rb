2ddce54bb583976a1ee8fcf4e5f4f0377b7cb322
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-09 21:20:15 +0000

    RED: returns buzz if num div by 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,6 +6,10 @@ describe FizzBuzz do
     it 'returns Fizz if number is divisible by 3' do
       expect(subject.play(3)).to eq 'Fizz'
     end
+
+    it 'returns Buzz if number is divisible by 5' do
+      expect(subject.play(5)).to eq 'Buzz'
+    end
   end
 
 end
