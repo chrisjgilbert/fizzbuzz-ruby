f084a022c94caf97c0c35da9ddeb2cace885525b
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-09 21:28:00 +0000

    REFACTORED: is div by 3 into private helper method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -5,7 +5,7 @@ class FizzBuzz
       'FizzBuzz'
     elsif is_divisible_by_5(number)
       'Buzz'
-    elsif number % 3 == 0
+    elsif is_divisible_by_3(number)
       'Fizz'
     else
       number
@@ -20,6 +20,10 @@ class FizzBuzz
 
     def is_divisible_by_15(number)
       number % 15 == 0
+    end
+
+    def is_divisible_by_3(number)
+      number % 3 == 0
     end
 
 end
