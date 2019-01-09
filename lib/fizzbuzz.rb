adcfec70aeccea9b493cba280a785463404bba0e
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-09 21:23:08 +0000

    REFACTOR: is_divisible_by_5 private helper method addded

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,17 @@
 class FizzBuzz
 
   def play(number)
-    if number % 5 == 0
+    if is_divisible_by_5(number)
       'Buzz'
-    else 
+    else
       'Fizz'
     end
   end
+
+  private
+
+    def is_divisible_by_5(number)
+      number % 5 == 0
+    end
 
 end
